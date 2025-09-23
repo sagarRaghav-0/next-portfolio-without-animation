0ecb51e037a6164e1ce743de0cc92a1079a0507e
raghav-000 <rajpr071220@gamil.com>
2025-09-23 20:55:44 +0530

    adding dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="fixed top-0 left-0 w-full py-7 flex justify-between items-center  z-50 bg-[#efefef] dark:text-[#efeded] dark:bg-[transparent]  px-5 md:px-10">
+            <nav className="fixed top-0 left-0 w-full py-7 flex justify-between items-center   z-50 bg-[#efefef] dark:text-[#efeded] dark:bg-[transparent]  px-5 md:px-10">
                 {/* Logo */}
                 <Link href="/">
                     <h1 className="tracking-widest md:text-2xl">Sagar Raghav</h1>
@@ -27,12 +27,12 @@ const Navbar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex md:hidden items-center">
-                    <p className="bg-[#efefef] py-2 px-4 cursor-pointer">
+                    <p className="bg-[#efefef] dark:bg-[#797878aa]   py-3 px-4 cursor-pointer">
                         <FaPlay />
                     </p>
                     <button
                         onClick={() => setIsOpen(!isOpen)}
-                        className="bg-[#fcfcfc] py-2 px-4"
+                        className="bg-[#fcfcfc] dark:bg-[#797878aa]  py-2 px-4"
                     >
                         {isOpen ? 'Close' : 'Menu'}
                     </button>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
             {/* Fullscreen Mobile Menu with transition */}
             <div
-                className={`fixed inset-0 bg-[#efefef] px-5 flex flex-col items-start justify-center gap-8 z-40 transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
+                className={`fixed inset-0 bg-[#efefef] dark:text-[#efeded] dark:bg-[#797878]  px-5 flex flex-col items-start justify-center gap-8 z-40 transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
                     }`}
             >
                 {[
